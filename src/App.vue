2ed9f2e8a279d3c38aace51cb1068a024a9fd274
Ahmadreza Ahmadi <ahmadrezaahmadi.dev@gmail.com>
2023-07-29 23:24:59 +0330

    style: add and configure the font

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import TheHeader from '@/components/TheHeader.vue'
 </script>
 
 <template>
+  <TheHeader />
+
   <RouterView />
 </template>
+
+<style>
+body {
+  font-family: Inter, 'sans-serif';
+}
+</style>
