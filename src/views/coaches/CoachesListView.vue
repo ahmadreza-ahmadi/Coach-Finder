8fe17f0594e55bd30afa3c0ed7747faa4f57566b
Ahmadreza Ahmadi <ahmadrezaahmadi.dev@gmail.com>
2023-07-30 13:38:46 +0330

    feat: display the coaches in the coaches list

--- src/views/coaches/CoachesListView.vue
+++ src/views/coaches/CoachesListView.vue
@@ -1,5 +1,14 @@
 <script setup>
+import { computed } from 'vue'
 import { RouterLink } from 'vue-router'
+import { storeToRefs } from 'pinia'
+import { useCoachesStore } from '../../stores/coaches'
+
+const { coaches } = storeToRefs(useCoachesStore())
+
+const filteredCoaches = computed(() => {
+  return coaches.value
+})
 </script>
 
 <template>
@@ -9,8 +18,11 @@ import { RouterLink } from 'vue-router'
       <button>Refresh</button>
       <RouterLink to="/register">Register as Coach</RouterLink>
     </div>
-    <ul>
-      LIST OF COACHES
+    <ul v-if="filteredCoaches.length">
+      <li v-for="coach in filteredCoaches" :key="coach.id">
+        <p>{{ coach.firstName }}</p>
+      </li>
     </ul>
+    <p v-else>No coach found!</p>
   </section>
 </template>
