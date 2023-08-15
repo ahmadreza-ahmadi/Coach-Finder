81187415c34f4c05f57c8d7995b823f6889c68d5
Ahmadreza Ahmadi <ahmadrezaahmadi.dev@gmail.com>
2023-08-16 01:24:35 +0330

    feat: get and set requests to firebase

--- src/stores/requests.js
+++ src/stores/requests.js
@@ -1,6 +1,7 @@
 import { defineStore, storeToRefs } from "pinia";
 import { ref, computed } from "vue";
 import { useAuthStore } from '@/stores/auth.js'
+import axios from "axios";
 
 const authStore = useAuthStore()
 
@@ -12,17 +13,53 @@ export const useRequestsStore = defineStore('requests', () => {
   const filteredRequests = computed(() => requests.value.filter((request) => request.userId === userId.value))
 
   const hasRequests = computed(() => {
-    return filteredRequests.value && filteredRequests.value.length > 0
+    return requests.value && requests.value.length > 0
   })
 
-  const addRequest = (requestData) => {
+  const addRequest = async (requestData) => {
     const newRequest = {
-      id: Date.now().toString(36),
-      ...requestData
+      email: requestData.email,
+      message: requestData.message
     }
+
+    const response = await axios.post(`https://vue-http-demo-f1200-default-rtdb.firebaseio.com/requests/${requestData.coachId}.json`, newRequest)
+    const responseData = await response.data
+
+    if (response.statusText !== 'OK') {
+      // Error handling
+    }
+
+    newRequest.id = responseData.name
+    newRequest.coachId = requestData.coachId
 
     requests.value.unshift(newRequest)
   }
 
-  return { requests, filteredRequests, hasRequests, addRequest }
+  const setRequests = (requestList) => {
+    requests.value = requestList
+  }
+
+  const fetchRequests = async () => {
+    const response = await axios.get(`https://vue-http-demo-f1200-default-rtdb.firebaseio.com/requests/${userId.value}.json`)
+    const responseData = await response.data
+
+    if (response.statusText !== 'OK') {
+      // Error
+    }
+
+    const formattedRequests = []
+    for (const key in responseData) {
+      const request = {
+        coachId: userId,
+        id: key,
+        email: responseData[key].email,
+        message: responseData[key].message
+      }
+      formattedRequests.push(request)
+    }
+
+    setRequests(formattedRequests)
+  }
+
+  return { requests, filteredRequests, hasRequests, addRequest, fetchRequests }
 })
--- src/views/requests/RequestsReceivedView.vue
+++ src/views/requests/RequestsReceivedView.vue
@@ -1,11 +1,25 @@
 <script setup>
+import { ref, onMounted } from 'vue'
 import { storeToRefs } from 'pinia'
 import { useRequestsStore } from '@/stores/requests'
 import BaseContainer from '@/components/BaseContainer.vue'
+import BaseSpinner from '@/components/BaseSpinner.vue'
 import RequestItem from '@/components/RequestItem.vue'
 
 const requestsStore = useRequestsStore()
-const { filteredRequests, hasRequests } = storeToRefs(requestsStore)
+const { requests, hasRequests } = storeToRefs(requestsStore)
+
+const isLoading = ref(false)
+
+const loadRequests = async () => {
+  isLoading.value = true
+  await requestsStore.fetchRequests()
+  isLoading.value = false
+}
+
+onMounted(() => {
+  loadRequests()
+})
 </script>
 
 <template>
@@ -14,8 +28,9 @@ const { filteredRequests, hasRequests } = storeToRefs(requestsStore)
       <header class="text-center">
         <h2 class="text-3xl font-bold">Requests Recieved</h2>
       </header>
-      <ul v-if="hasRequests">
-        <RequestItem v-for="request in filteredRequests" :key="request.id" :request="request" />
+      <BaseSpinner v-if="isLoading" />
+      <ul v-else-if="hasRequests">
+        <RequestItem v-for="request in requests" :key="request.id" :request="request" />
       </ul>
       <h3 v-else>You haven't recieved any requests yet!</h3>
     </BaseContainer>
