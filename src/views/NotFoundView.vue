80a4a322bbf8420d3da68b42d56c965bf635937b
Ahmadreza Ahmadi <ahmadrezaahmadi.dev@gmail.com>
2023-08-16 14:32:12 +0330

    feat: implement "Not Found" page.

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,3 +1,16 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+import BaseContainer from '@/components/BaseContainer.vue'
+</script>
+
 <template>
-  <p>Not Found View</p>
+  <BaseContainer>
+    <section class="text-center">
+      <h2 class="text-3xl font-bold mb-3">Page not found</h2>
+      <p class="text-lg">
+        This page could not be found. Maybe check out all our
+        <RouterLink to="/coaches" class="text-blue-500">coaches</RouterLink>.
+      </p>
+    </section>
+  </BaseContainer>
 </template>
